1b2644e30e9354d26f6484ead57c25866c884b30
Caleb Bryant <70673772+calebryant@users.noreply.github.com>
2026-09-01 02:23:45 +0000

    generate go parser code

---
+++ antlr/chroniclelogstashparser_base_listener.go
@@ -0,0 +1,136 @@
+// Code generated from ChronicleLogstashParser.g4 by ANTLR 4.13.2. DO NOT EDIT.
+
+package parser // ChronicleLogstashParser
+
+import "github.com/antlr4-go/antlr/v4"
+
+// BaseChronicleLogstashParserListener is a complete listener for a parse tree produced by ChronicleLogstashParser.
+type BaseChronicleLogstashParserListener struct{}
+
+var _ ChronicleLogstashParserListener = &BaseChronicleLogstashParserListener{}
+
+// VisitTerminal is called when a terminal node is visited.
+func (s *BaseChronicleLogstashParserListener) VisitTerminal(node antlr.TerminalNode) {}
+
+// VisitErrorNode is called when an error node is visited.
+func (s *BaseChronicleLogstashParserListener) VisitErrorNode(node antlr.ErrorNode) {}
+
+// EnterEveryRule is called when any rule is entered.
+func (s *BaseChronicleLogstashParserListener) EnterEveryRule(ctx antlr.ParserRuleContext) {}
+
+// ExitEveryRule is called when any rule is exited.
+func (s *BaseChronicleLogstashParserListener) ExitEveryRule(ctx antlr.ParserRuleContext) {}
+
+// EnterFilterblock is called when production filterblock is entered.
+func (s *BaseChronicleLogstashParserListener) EnterFilterblock(ctx *FilterblockContext) {}
+
+// ExitFilterblock is called when production filterblock is exited.
+func (s *BaseChronicleLogstashParserListener) ExitFilterblock(ctx *FilterblockContext) {}
+
+// EnterConditionalblock is called when production conditionalblock is entered.
+func (s *BaseChronicleLogstashParserListener) EnterConditionalblock(ctx *ConditionalblockContext) {}
+
+// ExitConditionalblock is called when production conditionalblock is exited.
+func (s *BaseChronicleLogstashParserListener) ExitConditionalblock(ctx *ConditionalblockContext) {}
+
+// EnterStatement is called when production statement is entered.
+func (s *BaseChronicleLogstashParserListener) EnterStatement(ctx *StatementContext) {}
+
+// ExitStatement is called when production statement is exited.
+func (s *BaseChronicleLogstashParserListener) ExitStatement(ctx *StatementContext) {}
+
+// EnterExpression is called when production expression is entered.
+func (s *BaseChronicleLogstashParserListener) EnterExpression(ctx *ExpressionContext) {}
+
+// ExitExpression is called when production expression is exited.
+func (s *BaseChronicleLogstashParserListener) ExitExpression(ctx *ExpressionContext) {}
+
+// EnterUnary_expression is called when production unary_expression is entered.
+func (s *BaseChronicleLogstashParserListener) EnterUnary_expression(ctx *Unary_expressionContext) {}
+
+// ExitUnary_expression is called when production unary_expression is exited.
+func (s *BaseChronicleLogstashParserListener) ExitUnary_expression(ctx *Unary_expressionContext) {}
+
+// EnterBinary_expression is called when production binary_expression is entered.
+func (s *BaseChronicleLogstashParserListener) EnterBinary_expression(ctx *Binary_expressionContext) {}
+
+// ExitBinary_expression is called when production binary_expression is exited.
+func (s *BaseChronicleLogstashParserListener) ExitBinary_expression(ctx *Binary_expressionContext) {}
+
+// EnterExpression_val is called when production expression_val is entered.
+func (s *BaseChronicleLogstashParserListener) EnterExpression_val(ctx *Expression_valContext) {}
+
+// ExitExpression_val is called when production expression_val is exited.
+func (s *BaseChronicleLogstashParserListener) ExitExpression_val(ctx *Expression_valContext) {}
+
+// EnterMath_statement is called when production math_statement is entered.
+func (s *BaseChronicleLogstashParserListener) EnterMath_statement(ctx *Math_statementContext) {}
+
+// ExitMath_statement is called when production math_statement is exited.
+func (s *BaseChronicleLogstashParserListener) ExitMath_statement(ctx *Math_statementContext) {}
+
+// EnterMath_expression is called when production math_expression is entered.
+func (s *BaseChronicleLogstashParserListener) EnterMath_expression(ctx *Math_expressionContext) {}
+
+// ExitMath_expression is called when production math_expression is exited.
+func (s *BaseChronicleLogstashParserListener) ExitMath_expression(ctx *Math_expressionContext) {}
+
+// EnterBoolean_op is called when production boolean_op is entered.
+func (s *BaseChronicleLogstashParserListener) EnterBoolean_op(ctx *Boolean_opContext) {}
+
+// ExitBoolean_op is called when production boolean_op is exited.
+func (s *BaseChronicleLogstashParserListener) ExitBoolean_op(ctx *Boolean_opContext) {}
+
+// EnterBoolean_eval is called when production boolean_eval is entered.
+func (s *BaseChronicleLogstashParserListener) EnterBoolean_eval(ctx *Boolean_evalContext) {}
+
+// ExitBoolean_eval is called when production boolean_eval is exited.
+func (s *BaseChronicleLogstashParserListener) ExitBoolean_eval(ctx *Boolean_evalContext) {}
+
+// EnterPlugin is called when production plugin is entered.
+func (s *BaseChronicleLogstashParserListener) EnterPlugin(ctx *PluginContext) {}
+
+// ExitPlugin is called when production plugin is exited.
+func (s *BaseChronicleLogstashParserListener) ExitPlugin(ctx *PluginContext) {}
+
+// EnterKeyvalue is called when production keyvalue is entered.
+func (s *BaseChronicleLogstashParserListener) EnterKeyvalue(ctx *KeyvalueContext) {}
+
+// ExitKeyvalue is called when production keyvalue is exited.
+func (s *BaseChronicleLogstashParserListener) ExitKeyvalue(ctx *KeyvalueContext) {}
+
+// EnterKv_lvalue is called when production kv_lvalue is entered.
+func (s *BaseChronicleLogstashParserListener) EnterKv_lvalue(ctx *Kv_lvalueContext) {}
+
+// ExitKv_lvalue is called when production kv_lvalue is exited.
+func (s *BaseChronicleLogstashParserListener) ExitKv_lvalue(ctx *Kv_lvalueContext) {}
+
+// EnterKv_rvalue is called when production kv_rvalue is entered.
+func (s *BaseChronicleLogstashParserListener) EnterKv_rvalue(ctx *Kv_rvalueContext) {}
+
+// ExitKv_rvalue is called when production kv_rvalue is exited.
+func (s *BaseChronicleLogstashParserListener) ExitKv_rvalue(ctx *Kv_rvalueContext) {}
+
+// EnterHash is called when production hash is entered.
+func (s *BaseChronicleLogstashParserListener) EnterHash(ctx *HashContext) {}
+
+// ExitHash is called when production hash is exited.
+func (s *BaseChronicleLogstashParserListener) ExitHash(ctx *HashContext) {}
+
+// EnterList is called when production list is entered.
+func (s *BaseChronicleLogstashParserListener) EnterList(ctx *ListContext) {}
+
+// ExitList is called when production list is exited.
+func (s *BaseChronicleLogstashParserListener) ExitList(ctx *ListContext) {}
+
+// EnterIf_list is called when production if_list is entered.
+func (s *BaseChronicleLogstashParserListener) EnterIf_list(ctx *If_listContext) {}
+
+// ExitIf_list is called when production if_list is exited.
+func (s *BaseChronicleLogstashParserListener) ExitIf_list(ctx *If_listContext) {}
+
+// EnterList_value is called when production list_value is entered.
+func (s *BaseChronicleLogstashParserListener) EnterList_value(ctx *List_valueContext) {}
+
+// ExitList_value is called when production list_value is exited.
+func (s *BaseChronicleLogstashParserListener) ExitList_value(ctx *List_valueContext) {}
---
+++ antlr/chroniclelogstashparser_listener.go
@@ -0,0 +1,124 @@
+// Code generated from ChronicleLogstashParser.g4 by ANTLR 4.13.2. DO NOT EDIT.
+
+package parser // ChronicleLogstashParser
+
+import "github.com/antlr4-go/antlr/v4"
+
+// ChronicleLogstashParserListener is a complete listener for a parse tree produced by ChronicleLogstashParser.
+type ChronicleLogstashParserListener interface {
+	antlr.ParseTreeListener
+
+	// EnterFilterblock is called when entering the filterblock production.
+	EnterFilterblock(c *FilterblockContext)
+
+	// EnterConditionalblock is called when entering the conditionalblock production.
+	EnterConditionalblock(c *ConditionalblockContext)
+
+	// EnterStatement is called when entering the statement production.
+	EnterStatement(c *StatementContext)
+
+	// EnterExpression is called when entering the expression production.
+	EnterExpression(c *ExpressionContext)
+
+	// EnterUnary_expression is called when entering the unary_expression production.
+	EnterUnary_expression(c *Unary_expressionContext)
+
+	// EnterBinary_expression is called when entering the binary_expression production.
+	EnterBinary_expression(c *Binary_expressionContext)
+
+	// EnterExpression_val is called when entering the expression_val production.
+	EnterExpression_val(c *Expression_valContext)
+
+	// EnterMath_statement is called when entering the math_statement production.
+	EnterMath_statement(c *Math_statementContext)
+
+	// EnterMath_expression is called when entering the math_expression production.
+	EnterMath_expression(c *Math_expressionContext)
+
+	// EnterBoolean_op is called when entering the boolean_op production.
+	EnterBoolean_op(c *Boolean_opContext)
+
+	// EnterBoolean_eval is called when entering the boolean_eval production.
+	EnterBoolean_eval(c *Boolean_evalContext)
+
+	// EnterPlugin is called when entering the plugin production.
+	EnterPlugin(c *PluginContext)
+
+	// EnterKeyvalue is called when entering the keyvalue production.
+	EnterKeyvalue(c *KeyvalueContext)
+
+	// EnterKv_lvalue is called when entering the kv_lvalue production.
+	EnterKv_lvalue(c *Kv_lvalueContext)
+
+	// EnterKv_rvalue is called when entering the kv_rvalue production.
+	EnterKv_rvalue(c *Kv_rvalueContext)
+
+	// EnterHash is called when entering the hash production.
+	EnterHash(c *HashContext)
+
+	// EnterList is called when entering the list production.
+	EnterList(c *ListContext)
+
+	// EnterIf_list is called when entering the if_list production.
+	EnterIf_list(c *If_listContext)
+
+	// EnterList_value is called when entering the list_value production.
+	EnterList_value(c *List_valueContext)
+
+	// ExitFilterblock is called when exiting the filterblock production.
+	ExitFilterblock(c *FilterblockContext)
+
+	// ExitConditionalblock is called when exiting the conditionalblock production.
+	ExitConditionalblock(c *ConditionalblockContext)
+
+	// ExitStatement is called when exiting the statement production.
+	ExitStatement(c *StatementContext)
+
+	// ExitExpression is called when exiting the expression production.
+	ExitExpression(c *ExpressionContext)
+
+	// ExitUnary_expression is called when exiting the unary_expression production.
+	ExitUnary_expression(c *Unary_expressionContext)
+
+	// ExitBinary_expression is called when exiting the binary_expression production.
+	ExitBinary_expression(c *Binary_expressionContext)
+
+	// ExitExpression_val is called when exiting the expression_val production.
+	ExitExpression_val(c *Expression_valContext)
+
+	// ExitMath_statement is called when exiting the math_statement production.
+	ExitMath_statement(c *Math_statementContext)
+
+	// ExitMath_expression is called when exiting the math_expression production.
+	ExitMath_expression(c *Math_expressionContext)
+
+	// ExitBoolean_op is called when exiting the boolean_op production.
+	ExitBoolean_op(c *Boolean_opContext)
+
+	// ExitBoolean_eval is called when exiting the boolean_eval production.
+	ExitBoolean_eval(c *Boolean_evalContext)
+
+	// ExitPlugin is called when exiting the plugin production.
+	ExitPlugin(c *PluginContext)
+
+	// ExitKeyvalue is called when exiting the keyvalue production.
+	ExitKeyvalue(c *KeyvalueContext)
+
+	// ExitKv_lvalue is called when exiting the kv_lvalue production.
+	ExitKv_lvalue(c *Kv_lvalueContext)
+
+	// ExitKv_rvalue is called when exiting the kv_rvalue production.
+	ExitKv_rvalue(c *Kv_rvalueContext)
+
+	// ExitHash is called when exiting the hash production.
+	ExitHash(c *HashContext)
+
+	// ExitList is called when exiting the list production.
+	ExitList(c *ListContext)
+
+	// ExitIf_list is called when exiting the if_list production.
+	ExitIf_list(c *If_listContext)
+
+	// ExitList_value is called when exiting the list_value production.
+	ExitList_value(c *List_valueContext)
+}
